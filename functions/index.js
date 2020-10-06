2e3039e97e691343eb517c6b26f4abc911a34b6a
Adam Koprowski <Adam.Koprowski@gmail.com>
2020-10-06 08:30:22 +0100

    Adding Firebase setup with a regularly run function.

---
+++ functions/index.js
@@ -0,0 +1,6 @@
+const functions = require('firebase-functions');
+
+exports.scheduledFunction = functions.pubsub.schedule('every 5 minutes').onRun((context) => {
+  console.log('This will be run every 5 minutes!');
+  return null;
+});
