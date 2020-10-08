bd84eb9dbdc9e8b8b2fc43c261bf13619fcf5026
Adam Koprowski <Adam.Koprowski@gmail.com>
2020-10-08 19:45:35 +0100

    Creating Firestore with names of all the contests.

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,22 @@
-const functions = require('firebase-functions');
+'use strict';
 
-exports.scheduledFunction = functions.pubsub.schedule('every 5 minutes').onRun((context) => {
-  console.log('This will be run every 5 minutes!');
-  return null;
+const functions = require('firebase-functions');
+const axios = require('axios');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.loadContests = functions.https.onRequest(async (req, res) => {
+    const response = await axios.get('https://codeforces.com/api/contest.list?gym=false');
+    console.log(`Fetched # contests: ${response.data.result.length}`);
+    await Promise.all(response.data.result.map((item, index) => loadContest(item)));
+    res.json({result: 'OK'});
 });
+
+function loadContest(contest) {
+    console.log(`Processing contest: ${contest.id}`);
+    return db.collection('contests').doc(contest.id.toString()).set({
+        name: contest.name
+    });
+}
