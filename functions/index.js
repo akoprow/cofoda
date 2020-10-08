c2d227e22d81dce205330f47496a54e09c906ea5
Adam Koprowski <Adam.Koprowski@gmail.com>
2020-10-08 22:01:02 +0100

    Creating Firestore functions to fetch all problems and contests data.

--- functions/index.js
+++ functions/index.js
@@ -3,20 +3,49 @@
 const functions = require('firebase-functions');
 const axios = require('axios');
 const admin = require('firebase-admin');
+const async = require("async");
 
 admin.initializeApp();
 const db = admin.firestore();
 
-exports.loadContests = functions.https.onRequest(async (req, res) => {
-    const response = await axios.get('https://codeforces.com/api/contest.list?gym=false');
-    console.log(`Fetched # contests: ${response.data.result.length}`);
-    await Promise.all(response.data.result.map((item, index) => loadContest(item)));
-    res.json({result: 'OK'});
+exports.loadData = functions.runWith({timeoutSeconds: 540}).https.onRequest(async (req, res) => {
+  //await loadAllContests();
+  await loadAllProblems();
+  es.json({result: 'OK'});
 });
 
-function loadContest(contest) {
-    console.log(`Processing contest: ${contest.id}`);
-    return db.collection('contests').doc(contest.id.toString()).set({
-        name: contest.name
+async function loadAllContests() {
+  const response = await axios.get('https://codeforces.com/api/contest.list?gym=false');
+
+  console.log(`Fetched contests #: ${response.data.result.length}`);
+  const newContests = await async.map(response.data.result, (contest) => loadContest(contest));
+  const numNewContests = newContests.reduce((a, b) => a + b, 0);
+  console.log(`Processed new contests #: ${numNewContests}`);
+}
+
+async function loadContest(contest) {
+  const contestRef = db.collection('contests').doc(contest.id.toString())
+  if (!contestRef.exists) {
+    console.log(`Found new contest: ${contest.id}`);
+    await contestRef.set({
+      name: contest.name
     });
+    console.log(`Finished processing new contest: ${contest.id}`);
+  }
+}
+
+async function loadAllProblems() {
+  const response = await axios.get('https://codeforces.com/api/problemset.problems');
+  console.log(`Fetched # problems: ${response.data.result.problems.length}`);
+  await async.each(response.data.result.problems, (problem) => loadProblem(problem));
+}
+
+async function loadProblem(problem) {
+  const problemId = problem.contestId + problem.index;
+  const problemRef = db.collection('problems').doc(problemId);
+  if (!problemRef.exists) {
+    console.log(`Found new problem: ${problemId}`);
+    await problemRef.set(problem);
+    console.log(`Finished processing new problem: ${problemId}`);
+  }
 }
